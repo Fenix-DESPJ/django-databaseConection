2153b8737a62e73a44d2928b3cf572d3354381d0
Steban <Juanesspa76@gmail.com>
2026-06-24 06:51:47 -0500

    fix Render agenda

--- servicios/urls.py
+++ servicios/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('individual/', views.servicios_ind, name='servicios_ind'),
     path('paquete-1/', views.servicios_paq, name='servicios_paq_1'),
     path('paquete-2/', views.servicios_paq_2, name='servicios_paq_2'),
+    path('agenda/', views.agenda_view, name='agenda'),
     # Mantén esta redirección si la necesitas
     path('paquetes/1/', RedirectView.as_view(url='/servicios/paquete-1/')),
 ]
