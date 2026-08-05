34ccf11b0f640b188fc44dfcae5de268c2cb0129
Steban <Juanesspa76@gmail.com>
2026-08-05 10:49:02 -0500

    Bootstrap local añadido al static

--- barbershopmya/settings.py
+++ barbershopmya/settings.py
@@ -164,6 +164,7 @@ GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
